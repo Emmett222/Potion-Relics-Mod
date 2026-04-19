32d20102350775cb2e38a1d5962486733cb4177b
Emmett Grebe <emmettgrebe@gmail.com>
2026-04-19 19:25:12 -0400

    Added night vision relic to moditems

--- src/main/java/net/emmett222/potionrelicsmod/items/ModItems.java
+++ src/main/java/net/emmett222/potionrelicsmod/items/ModItems.java
@@ -1,6 +1,7 @@
 package net.emmett222.potionrelicsmod.items;
 
 import net.emmett222.potionrelicsmod.PotionRelicsMod;
+import net.emmett222.potionrelicsmod.items.relics.NightVisionRelic;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,7 +13,7 @@ public class ModItems {
         DeferredRegister.create(ForgeRegistries.ITEMS, PotionRelicsMod.MOD_ID);
 
     public static final RegistryObject<Item> NIGHTVISIONRELIC = ITEMS.register("nightvisionrelic", 
-        () -> new Item(new Item.Properties()));
+        () -> new NightVisionRelic(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
