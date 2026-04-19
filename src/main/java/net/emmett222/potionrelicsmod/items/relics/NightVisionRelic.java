b33780eb7ed06e862f133e6c49eee913a6c1cc0c
Emmett Grebe <emmettgrebe@gmail.com>
2026-04-19 18:57:49 -0400

    Made the night vision relic.

---
+++ src/main/java/net/emmett222/potionrelicsmod/items/relics/NightVisionRelic.java
@@ -0,0 +1,23 @@
+package net.emmett222.potionrelicsmod.items.relics;
+
+import net.minecraft.world.effect.MobEffects;
+
+/**
+ * Night Vision Relic. Gives the player constant night vision if in the inventory.
+ * 
+ * @author Emmett Grebe
+ * @version 4-19-2026
+ */
+public class NightVisionRelic extends BaseRelic{
+
+    /**
+     * Explicit constructor.
+     * Sets the effect to Night Vision.
+     * 
+     * @param pProperties The pProperties to be used.
+     */
+    public NightVisionRelic(Properties pProperties) {
+        super(pProperties, MobEffects.NIGHT_VISION);
+    }
+    
+}
